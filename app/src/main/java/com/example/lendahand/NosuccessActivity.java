24601d111e4d4bcfa7495bc9e0234ee51ef67808
KaraboMac <2809967@students.wits.ac.za>
2025-05-17 23:14:26 +0200

    Added success and noSuccess

---
+++ app/src/main/java/com/example/lendahand/NosuccessActivity.java
@@ -0,0 +1,23 @@
+package com.example.lendahand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class NosuccessActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_nosuccess); // Make sure this matches your XML file
+
+        Button returnHome = findViewById(R.id.returnHomeButton); // Now it's safe
+        returnHome.setOnClickListener(v -> {
+            Intent intent = new Intent(NosuccessActivity.this, DonateActivity.class);
+            intent.putExtra("userID", getIntent().getStringExtra("userID"));
+            intent.putExtra("fullName", getIntent().getStringExtra("fullName"));
+            startActivity(intent);
+            finish();
+        });
+    }
+}
